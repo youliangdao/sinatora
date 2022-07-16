a6013872207d51669c14c55e146b94c6e1082e82
youliangdao <youliangdaojing5@gmail.com>
2022-07-16 23:11:32 +0900

    ユーザー新規作成機能の追加

--- Gemfile
+++ Gemfile
@@ -4,4 +4,6 @@ source "https://rubygems.org"
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}"}
 gem 'sinatra'
 gem 'sinatra-contrib'
+gem 'sqlite3'
+gem 'sinatra-activerecord'
 # gem "rails"
--- app.rb
+++ app.rb
@@ -1,8 +1,40 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+get '/' do
+  "hello world!"
+end
 
 get '/learn-link-to' do
   erb :learn_link_to
+end
+
+get '/learn-layout' do
+  erb :learn_layout
+end
+
+get '/users' do
+  db = SQLite3::Database.new 'sample.db'
+  rs = db.execute('select * from users;')
+  @users_hash = rs.map do |row|
+    {id: row[0], name: row[1]}
+  end
+  erb :'users/index'
+end
+
+get '/users/new' do
+  erb :'users/new'
+end
+
+post '/users' do
+  db = SQLite3::Database.new 'sample.db'
+  sql = 'insert into users(name) values(?);'
+  stmt = db.prepare(sql)
+  stmt.bind_params(params[:name])
+  stmt.execute
+
+  redirect to('/users')
 end
 
 def link_to(text, url)
